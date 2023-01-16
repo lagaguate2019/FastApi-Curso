5d7bcfc27096f5de2b7dadafaaee591e781e0deb
Luis Gutierrez <lagaguate@gmail.com>
2023-01-16 14:05:55 -0600

    Clase para representar un producto

--- practicas_activas/api_productos.py
+++ practicas_activas/api_productos.py
@@ -1,4 +1,16 @@
+"""
+Fecha: 2023-01-16
+Tutorial: Crear CRUD, con fastAPI
+"""
+
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+"""
+Defincion de modelo productos
+"""
+class Producto (BaseModel):
+    id: Optional [str]
 
 app = FastAPI()
 
