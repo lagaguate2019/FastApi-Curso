a54d82d573c9775aadca1c3ab00d98128ad33ec6
Luis Gutierrez <lagaguate@gmail.com>
2023-01-16 16:56:39 -0600

    metodo eliminar

--- practicas_activas/api_productos.py
+++ practicas_activas/api_productos.py
@@ -4,7 +4,7 @@ Tutorial: Crear CRUD, con fastAPI
 """
 
 from typing import Optional
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from uuid import uuid4 as uuid
 """
@@ -35,6 +35,8 @@ def crear_producto (producto: Producto):
     productos.append(producto)
     return {'message':'Creado'}
 
+
+## Busqueda por for, if
 @app.get("/producto/{id}")
 def getProductoPorId(producto_id: str):
     for p in productos:
@@ -42,3 +44,27 @@ def getProductoPorId(producto_id: str):
             return p
 
     return {'mensaje':f'No existe id {producto_id}'} 
+
+## Busqueda por funcion filter  
+@app.get('/productofilter/{id}')
+def getProductoPorId_filter (id:str):
+    result = list(filter (lambda p: p.id == id, productos))
+
+    if len(result):
+        return result
+
+    raise HTTPException(status_code=404,detail=f'No existe id {id}')
+
+ ## Borar Id
+@app.delete('/producto/{id}')
+def borrarId (id:str):
+    result = list(filter (lambda p: p.id == id, productos))
+
+    if len(result):
+        producto = result[0]
+        productos.remove(producto)
+        return {'mensaje':f'{id}, eliminado'}
+
+    raise HTTPException(status_code=404,detail=f'No existe id {id}')
+
+
