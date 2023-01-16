f5c493a64fdf5b9d47184e27917894378f62030e
Luis Gutierrez <lagaguate@gmail.com>
2023-01-16 15:43:49 -0600

    agregar registro metodo post

--- practicas_activas/api_productos.py
+++ practicas_activas/api_productos.py
@@ -6,7 +6,7 @@ Tutorial: Crear CRUD, con fastAPI
 from typing import Optional
 from fastapi import FastAPI
 from pydantic import BaseModel
-
+from uuid import uuid4 as uuid
 """
 Defincion de modelo productos
 """
@@ -14,11 +14,23 @@ class Producto (BaseModel):
     id: Optional [str]
     nombre: str
     precio_venta: float
-    precio_compra:float
+    precio_compra: float
     proveedor: str
 
 app = FastAPI()
 
+productos = []
+
 @app.get("/")
 def index():
     return {'index':'Bienvenidos - API Products'}
+
+@app.get("/producto")
+def getProducto():
+    return productos
+
+@app.post("/producto")
+def crear_producto (producto: Producto):
+    producto.id = str(uuid())
+    productos.append(producto)
+    return {'message':'Creado'}
